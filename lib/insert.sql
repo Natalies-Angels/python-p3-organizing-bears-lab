a389496187994e4ff8102755892f3deda8c6658b
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-11-02 08:05:08 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'White', 'Cheery', 1),
+  ('Tabitha', 1, 'F', 'Black', 'Shy', 0),
+  ('Sergeant Brown', 4, 'M', 'Grey', 'Grumpy', 1),
+  ('Melissa', 6, 'F', 'Blonde', 'Shy', 1),
+  ('Grinch', 10, 'M', 'Green', 'Grumpy', 1),
+  ('Wendy', 3, 'F', 'Black', 'Despodent', 1),
+  (null, 20, 'M', 'black', 'aggressive', 0);
